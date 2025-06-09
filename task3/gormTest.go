5e733de79dde2b043582182efe3b105fa6c66792
zhangxx-rabbit <zhang_xinxian@icloud.com>
2025-06-10 00:37:17 +0800

    升级go版本

---
+++ task3/gormTest.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+type User struct {
+	ID   uint
+	Name string
+	Age  int
+}
+
+func main() {
+	// 连接 SQLite，数据库文件为当前目录下的 test.db
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		log.Fatal("连接 SQLite 失败:", err)
+	}
+
+	// 自动创建表
+	db.AutoMigrate(&User{})
+
+	// 增删改查操作
+	user := User{Name: "Alice", Age: 30}
+	db.Create(&user)
+
+	var result User
+	db.First(&result, 1)
+	log.Println("查询结果:", result)
+}
